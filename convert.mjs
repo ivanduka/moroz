b54119e3e1eef6642fa97249ea5ac5796b727525
Ivan Duka <ivan@iduka.net>
2024-02-24 14:43:21 -0800

    preparation

---
+++ convert.mjs
@@ -0,0 +1,20 @@
+import fs from "fs/promises";
+import path from "path";
+import { glob } from "glob";
+import { fileURLToPath } from "url";
+import iconv from "iconv-lite";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+let htmlFiles = await glob("**/*.html", { ignore: "node_modules/**" });
+
+// htmlFiles = htmlFiles.slice(0, 10);
+
+for (const htmlFile of htmlFiles) {
+  const fileFullPath = path.join(__dirname, htmlFile);
+  const content = await fs.readFile(fileFullPath);
+  const decoded = iconv.decode(content, "koi8-r");
+  await fs.writeFile(fileFullPath, decoded);
+}
